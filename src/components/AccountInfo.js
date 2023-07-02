719983a6a0af6e86a678c79560e50021bcbb2b4a
SamSvetlik <sam.svetlik@gmail.com>
2023-07-02 16:06:08 -0500

    bugfix: better organization for AccountInfo component with syntactic elements

--- src/components/AccountInfo.js
+++ src/components/AccountInfo.js
@@ -1,24 +1,58 @@
+import { useState } from "react";
 
 
 const AccountInfo = (props) => {
     const {user} = props
+
+    const [updatedUser, setUpdate] = useState({user})
+    
+    const handleTextChange = (e) => {
+        const { name, value } = e.target;
+        setUpdate((prevState) => {
+        return {
+            ...prevState,
+            [name]: value,
+        };
+        });
+    };
+    // todo:
+    // set flag for edit on/off
+    // edit on = all fields become inputs,
+    // handleTextChange to update info in updatedUser
+    // back button to discard
+    // confirm button to post to backend/users
+
     return (
         <div>
-            <img src={user.profilePic} />
-            <h1>{user.name}</h1>
-            <h2>{user.beltRank}</h2>
-            <h3>{user.isAdmin ? "Instructor" : "Student"}</h3>
-            <h3>{user.beltProgress}</h3>
-            <h2>Membership</h2>
-            <h3>Days remaining: function to calculate</h3>
-            <h3>Member since: {user.memberSince}</h3>
-            <h2>General Information</h2>
-            <h3>Emergency Contact: {user.emergencyContact}</h3>
-            <h3>Emergency Number: {user.emergencyNumber}</h3>
-            <h3>Age: Function to calculate</h3>
-            <h3>Birthday: {user.birthday}</h3>
-            <h2>Notes</h2>
-            <p>{user.notes}</p>
+            <section>
+                <div className="profilePicContainer">
+                   <img src={user.profilePic} />
+                </div>
+                <div>
+                    <h1>{user.name}</h1>
+                    <h2>{user.beltRank}</h2>
+                    <h3>{user.isAdmin ? "Instructor" : "Student"}</h3>
+                    <p>{user.beltProgress}</p>
+                </div>
+            </section>
+            <section>
+                <div>
+                    <h2>Membership</h2>
+                    <p>Days remaining: function to calculate</p>
+                    <p>Member since: {user.memberSince}</p>
+                </div>
+                <div>
+                    <h2>General Information</h2>
+                    <p>Emergency Contact: {user.emergencyContact}</p>
+                    <p>Emergency Number: {user.emergencyNumber}</p>
+                    <p>Age: Function to calculate</p>
+                    <p>Birthday: {user.birthday}</p>
+                </div>
+            </section>
+            <section>
+                <h2>Notes</h2>
+                <p>{user.notes}</p>
+            </section>
         </div>
     )
 }
