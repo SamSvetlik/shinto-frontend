ba4bdaec743991e87c4dffffb10685341c2b1219
SamSvetlik <sam.svetlik@gmail.com>
2023-07-03 15:43:14 -0500

    chore: added react-datetime-picker to make event creation more user-friendly, formatted generated eventTime correctly

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -1,5 +1,9 @@
 import { useState } from "react"
 import axios from "axios"
+import DateTimePicker from "react-datetime-picker"
+import 'react-datetime-picker/dist/DateTimePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-clock/dist/Clock.css';
 
 const CreateEvent = (props) => {
     const {userId} = props
@@ -23,7 +27,7 @@ const CreateEvent = (props) => {
 
     const submitEvent = (e) => {
         e.preventDefault()
-        axios.post("https://shinto-backend.vercel.app/events", {...event})
+        axios.post("https://shinto-backend.vercel.app/events", {...event, eventTime: eventTime.toISOString().replaceAll("T", " ").slice(0, -5)})
             .then(res => {
                 console.log(JSON.parse(res.config.data))
                 alert("New event added to calendar!")
@@ -31,15 +35,19 @@ const CreateEvent = (props) => {
             .catch(err => console.log("error: ", err))
     }
 
+    const [eventTime, setEventTime] = useState(new Date())
+
+    
     return (
         <>
             <form className="eventCreater-form" onSubmit={submitEvent}>
                 <label>Event Name</label>
-                <input name="eventName" type="text" value={event.eventName} onChange={handleTextChange}></input>
+                <input name="eventName" type="text" value={event.eventName} onChange={handleTextChange} required></input>
                 <label>Description</label>
-                <input name="eventDescription" type="text" value={event.eventDescription} onChange={handleTextChange}></input>
+                <input name="eventDescription" type="text" value={event.eventDescription} onChange={handleTextChange} required></input>
                 <label>Date and Time</label>
-                <input name="eventTime" type="text" placeholder="2023-06-26 12:30:00" value={event.eventTime} onChange={handleTextChange}></input>
+                {/* <input name="eventTime" type="text" placeholder="2023-06-26 12:30:00" value={event.eventTime} onChange={handleTextChange}></input> */}
+                <DateTimePicker onChange={setEventTime} value={eventTime}  />
                 <input type="submit" value="submit"></input>
             </form>
         </>
